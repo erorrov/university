29535687f088f97da84715188508995be598af25
Egor Egorov <erorrov@gmail.com>
2017-10-01 22:48:40 +0300

    fixes

--- Course 1/ex2-binary-search/src/Main.java
+++ Course 1/ex2-binary-search/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,11 +12,16 @@ public class Main {
         final Scanner input = new Scanner(System.in);
 
         System.out.print("Array length: ");
-        final int[] array = bubbleSort(getRandomArray(input.nextInt()));
-        printArray("Array", array);
+        final int[] array = getRandomArray(input.nextInt());
+        System.out.println(Arrays.toString(array));
 
         System.out.print("To find: ");
-        search(array, input.nextInt());
+        int result = search(array, input.nextInt());
+        if (result == -1) {
+            System.out.println("Nothing found");
+        } else {
+            System.out.println("i = " + result);
+        }
     }
 
 
@@ -27,54 +33,27 @@ public class Main {
             randomArray[i] = rand.nextInt(MAX_ALLOWED_NUMBER);
         }
 
+        Arrays.sort(randomArray);
         return randomArray;
     }
 
 
-    private static void printArray(final String caption, final int[] array) {
-        System.out.print(caption + ":\t\t");
-
-        for (final int elem : array) {
-            System.out.print(elem + "\t");
-        }
-
-        System.out.println();
-    }
-
-
-    private static int[] bubbleSort(final int[] sArray) {
-        int[] bblArray = copyOf(sArray, sArray.length);
-        int tmp;
-
-        for (int i2 = 0; i2 < bblArray.length; i2++) {
-            for (int i = 0; i < bblArray.length - 1; i++) {
-                if (bblArray[i] > bblArray[i + 1]) {
-                    tmp = bblArray[i];
-                    bblArray[i] = bblArray[i + 1];
-                    bblArray[i + 1] = tmp;
-                }
-            }
-        }
-
-        return bblArray;
-    }
-
-
-    private static void search(final int[] array, final int search) {
+    private static int search(final int[] array, final int search) {
         int startBound = 0;
-        int endBound = array.length;
+        int endBound = array.length - 1;
         int middle = 0;
 
-        while (true) {
+        while (endBound >= startBound) {
             middle = (endBound + startBound) / 2;
             if (array[middle] == search) {
-                System.out.println("FOUND, i = " + middle);
-                break;
+                return middle;
             } else if (array[middle] < search) {
-                startBound = middle;
+                startBound = middle + 1;
             } else if (array[middle] > search) {
-                endBound = middle;
+                endBound = middle - 1;
             }
         }
+
+        return -1;
     }
 }
