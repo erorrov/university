40ffd0ab80298bdc34241188d176fe20b05c7c36
Egor Egorov <erorrov@gmail.com>
2017-11-02 20:38:25 +0300

    final code

--- Course 1/ex3-gui-text-editor/src/main/java/ru/erorrov/Editor.java
+++ Course 1/ex3-gui-text-editor/src/main/java/ru/erorrov/Editor.java
@@ -14,6 +14,7 @@ class Editor extends JFrame implements ActionListener, DocumentListener {
     private JMenuBar menuBar = new JMenuBar();
     private JTextArea textArea = new JTextArea();
     private File file;
+    public boolean changed = false;
 
     public Editor() {
         super("Text Editor");
@@ -83,7 +84,7 @@ class Editor extends JFrame implements ActionListener, DocumentListener {
         } else if (action.equals("Open")) {
             loadFile();
         } else if (action.equals("Save")) {
-            //
+            saveFile();
         } else if (action.equals("Author")) {
             //
         } else if (action.equals("Source code")) {
@@ -100,31 +101,90 @@ class Editor extends JFrame implements ActionListener, DocumentListener {
     }
 
     public void newFile() {
+        if (changed) {
+            int question = JOptionPane.showConfirmDialog(null,
+                    "Do you want to save it?", "Save file",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+            if (question == JOptionPane.YES_OPTION) {
+                saveFile();
+            }
+        }
+
         file = null;
         textArea.setText("");
         setTitle("Text Editor");
     }
 
     private void loadFile() {
-        
+        JFileChooser dialog = new JFileChooser(System.getProperty("user.home"));
+        dialog.setMultiSelectionEnabled(false);
+        try {
+            if (dialog.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                file = dialog.getSelectedFile();
+                textArea.setText(readFile(file));
+                setTitle(file.getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private String readFile(File file) throws IOException {
+        StringBuilder result = new StringBuilder();
+        FileReader fr = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fr);
 
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line + "\n");
+        }
 
-
+        return result.toString();
     }
+
+    private void saveFile() {
+        if (file == null) {
+            //save new file
+            JFileChooser dialog = new JFileChooser(System.getProperty("user.home"));
+            dialog.setDialogTitle("Save new file as...");
+            dialog.showSaveDialog(this);
+            file = dialog.getSelectedFile();
+        } else {
+            //save old file
+            setTitle(file.getName() + " (saved)");
+        }
+
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.write(textArea.getText());
+            writer.close();
+            changed = false;
+            setTitle(file.getName() + " (saved)");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     public void insertUpdate(DocumentEvent e) {
-
+        changed = true;
+        if (file != null) {
+            setTitle(file.getName() + " (changed)");
+        }
     }
 
     public void removeUpdate(DocumentEvent e) {
-
+        changed = true;
+        if (file != null) {
+            setTitle(file.getName() + " (changed)");
+        }
     }
 
     public void changedUpdate(DocumentEvent e) {
-
+        changed = true;
+        if (file != null) {
+            setTitle("Editor - " + file.getName() + " (changed)");
+        }
     }
 }
