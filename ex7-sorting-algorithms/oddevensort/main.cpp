733f435047943da89ac74f776649ae82a569a53e
Egor Egorov <erorrov@gmail.com>
2018-03-26 01:17:33 +0300

    odd-even sort homework

---
+++ ex7-sorting-algorithms/oddevensort/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    //Исхоный массив
+    int arr[] = {2, 28, 13, 37, 8, 80, 0, 55, 5, 53, 35};
+    //Длина массива
+    int arrLen = sizeof(arr) / sizeof(arr[0]);
+
+    //Сортировка
+    for (int i = 0; i < arrLen; i++) {
+        for (int j = (i % 2) ? 0 : 1; j < arrLen - 1; j += 2) {
+            if (arr[j] > arr[j + 1]) {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+
+    //Отображение массива
+    for (int i = 0; i < arrLen; i++) {
+        std::cout << arr[i] << "\t";
+    }
+
+    return 0;
+}
